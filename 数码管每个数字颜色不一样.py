69d3044e2cd1da4e2fe2c1a784594059e12e1eea
huajuqn <43066461+huajuqn@users.noreply.github.com>
2018-10-20 12:19:18 +0800

    数码管

---
+++ 数码管每个数字颜色不一样.py
@@ -0,0 +1,52 @@
+#e7.2DrawSevenSegDisplay.py
+import turtle, datetime
+ls=["red","blue","pink","#685632","#eeaeee","yellow","#f1e5e8","#aaf2e5"]
+def drawGap(): #绘制数码管间隔
+    turtle.penup()
+    turtle.fd(5)
+def drawLine(draw):   #绘制单段数码管
+    drawGap()
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    drawGap()    
+    turtle.right(90)
+def colorL(b):
+    turtle.pencolor(ls[b])
+def drawDigit(d): #根据数字绘制七段数码管
+    drawLine(True) if d in [2,3,4,5,6,8,9] else drawLine(False)
+    drawLine(True) if d in [0,1,3,4,5,6,7,8,9] else drawLine(False)
+    drawLine(True) if d in [0,2,3,5,6,8,9] else drawLine(False)
+    drawLine(True) if d in [0,2,6,8] else drawLine(False)
+    turtle.left(90)
+    drawLine(True) if d in [0,4,5,6,8,9] else drawLine(False)
+    drawLine(True) if d in [0,2,3,5,6,7,8,9] else drawLine(False)
+    drawLine(True) if d in [0,1,2,3,4,7,8,9] else drawLine(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20) 
+def drawDate(date):
+    c=0
+    for i in date:
+        if i == '-':
+            turtle.pencolor("#7D2BBC")
+            turtle.write('年',font=("Arial", 18, "normal"))
+            turtle.fd(40) 
+        elif i == '=':
+            turtle.pencolor("#96A5A2")
+            turtle.write('月',font=("Arial", 18, "normal"))
+            turtle.fd(40)
+        elif i == '+':
+            turtle.pencolor("#E26720")
+            turtle.write('日',font=("Arial", 18, "normal"))
+        else:
+            colorL(c)
+            drawDigit(eval(i))
+            c+=1
+def main():
+    turtle.setup(800, 350, 200, 200)
+    turtle.penup()
+    turtle.fd(-350)
+    turtle.pensize(5)
+    drawDate(datetime.datetime.now().strftime('%Y-%m=%d+'))
+    turtle.hideturtle()
+main() 
