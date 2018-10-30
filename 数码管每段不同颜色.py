0f5f48ea1becad560fec561d390e36c5c3d4c937
huajuqn <43066461+huajuqn@users.noreply.github.com>
2018-10-30 13:29:03 +0800

    Update 数码管每段不同颜色.py

--- 数码管每段不同颜色.py
+++ 数码管每段不同颜色.py
@@ -1,36 +1,37 @@
 #e7.2DrawSevenSegDisplay.py
 import turtle, datetime
+ls=["red","blue","pink","#685632","#eeaeee","yellow","#f1e5e8","#aaf2e5","#7D2BBC","#96A5A2","#E26720"]
+c=0
 def drawGap(): #绘制数码管间隔
     turtle.penup()
     turtle.fd(5)
+def colorL(b):
+    turtle.pencolor(ls[b])
 def drawLine(draw):   #绘制单段数码管
+    global c
     drawGap()
+    colorL(c)
     turtle.pendown() if draw else turtle.penup()
     turtle.fd(40)
+    c+=1
+    if c ==7:
+        c=0
     drawGap()    
     turtle.right(90)
 def drawDigit(d): #根据数字绘制七段数码管
-    turtle.pencolor("red")
     drawLine(True) if d in [2,3,4,5,6,8,9] else drawLine(False)
-    turtle.pencolor("blue")
     drawLine(True) if d in [0,1,3,4,5,6,7,8,9] else drawLine(False)
-    turtle.pencolor("yellow")
     drawLine(True) if d in [0,2,3,5,6,8,9] else drawLine(False)
-    turtle.pencolor("gold")
     drawLine(True) if d in [0,2,6,8] else drawLine(False)
     turtle.left(90)
-    turtle.pencolor("violet")
     drawLine(True) if d in [0,4,5,6,8,9] else drawLine(False)
-    turtle.pencolor("purple")
     drawLine(True) if d in [0,2,3,5,6,7,8,9] else drawLine(False)
-    turtle.pencolor("green")
     drawLine(True) if d in [0,1,2,3,4,7,8,9] else drawLine(False)
     turtle.pencolor("darkgreen")
     turtle.left(180)
     turtle.penup()
     turtle.fd(20) 
 def drawDate(date):
-    
     for i in date:
         if i == '-':
             turtle.write('年',font=("Arial", 18, "normal"))
