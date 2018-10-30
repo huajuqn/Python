a60b2d115ecf22867dcad7ecb1b4ae162b9e99bd
huajuqn <43066461+huajuqn@users.noreply.github.com>
2018-10-30 13:33:24 +0800

    Update 数码管每个数字颜色不一样.py

--- 数码管每个数字颜色不一样.py
+++ 数码管每个数字颜色不一样.py
@@ -1,6 +1,6 @@
 #e7.2DrawSevenSegDisplay.py
 import turtle, datetime
-ls=["red","blue","pink","#685632","#eeaeee","yellow","#f1e5e8","#aaf2e5"]
+ls=["red","blue","pink","#685632","#eeaeee","yellow","#f1e5e8","#aaf2e5","#7D2BBC","#96A5A2","#E26720"]
 def drawGap(): #绘制数码管间隔
     turtle.penup()
     turtle.fd(5)
@@ -10,7 +10,7 @@ def drawLine(draw):   #绘制单段数码管
     turtle.fd(40)
     drawGap()    
     turtle.right(90)
-def colorL(b):
+def colorL(b): #颜色改变
     turtle.pencolor(ls[b])
 def drawDigit(d): #根据数字绘制七段数码管
     drawLine(True) if d in [2,3,4,5,6,8,9] else drawLine(False)
@@ -28,16 +28,19 @@ def drawDate(date):
     c=0
     for i in date:
         if i == '-':
-            turtle.pencolor("#7D2BBC")
+            colorL(c)
             turtle.write('年',font=("Arial", 18, "normal"))
-            turtle.fd(40) 
+            turtle.fd(40)
+            c+=1
         elif i == '=':
-            turtle.pencolor("#96A5A2")
+            colorL(c)
             turtle.write('月',font=("Arial", 18, "normal"))
             turtle.fd(40)
+            c+=1
         elif i == '+':
-            turtle.pencolor("#E26720")
+            colorL(c)
             turtle.write('日',font=("Arial", 18, "normal"))
+            c+=1
         else:
             colorL(c)
             drawDigit(eval(i))
