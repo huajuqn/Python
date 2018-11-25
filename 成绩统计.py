32c29b71d853d4d4e0ac901f0435981e808f8ed3
huajuqn <43066461+huajuqn@users.noreply.github.com>
2018-11-25 15:27:45 +0800

    Update 成绩统计.py

--- 成绩统计.py
+++ 成绩统计.py
@@ -5,12 +5,14 @@ for line in fo:
     line = line.replace("\n","")
     ls.append(line.split(","))
 fo.close()
+
 #求总分
 sum=[0,0,0,0,0]
 for i in range(1,6):
     for b in range(1,4):
          sum[i-1]=sum[i-1]+eval(ls[i][b])
     sum[i-1]=str(sum[i-1])
+    
 #求最大值，最小值，平均分
 max=[0,0,0,""]
 min=[100,100,100,""]
@@ -25,18 +27,21 @@ for k in range(1,4):
     max[k-1]=str(max[k-1])
     min[k-1]=str(min[k-1])
     avg[k-1]='%.2f'%(avg[k-1])
+    
 #把总分写入列表
 ls[0].append("总分")
 for s in range(1,6):
     ls[s].append(sum[s-1])
-#把最大值，最小值，平均分写入列表
+    
+#输出最大值，最小值，平均分写入列表
 fls=["最高分","最低分","平均分"]
 max.insert(0,fls[0])
 min.insert(0,fls[1])
 avg.insert(0,fls[2])
 lt=[max,min,avg]
 for i in range(3):
-    ls.append(lt[i])
+    print(ls[0][i+1],max[0],max[i+1],min[0],min[i+1],avg[0],avg[i+1])
+
 #输出csv文件
 output = open('score.csv','w',encoding='gbk')
 for row in ls:
